aa76bec7c3a14f04ab5ba2de540bf62e82c6b4a4
xiayunhu <2390472338@qq.com>
2019-04-23 15:14:59 +0800

    Site updated: 2019-04-23 15:14:57

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,10 +1,10 @@
-/*ҳ���������ɺ󣬴������ư�ť*/
+/*ҳ��������ɺ󣬴������ư�ť*/
   !function (e, t, a) { 
     /* code */
     var initCopyCode = function(){
       var copyHtml = '';
       copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-      //fa fa-globe����ȥ�������滻�Լ���Ҫ��ͼ��
+      //fa fa-globe����ȥ������滻�Լ���Ҫ��ͼ��
 copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
       copyHtml += '</button>';
       $(".highlight .code pre").before(copyHtml);
